d78453a2fe34ce4d9a5590b65e0e6f69dc55ba1c
Daniele Di Bernardo <daniele.dib@gmail.com>
2014-08-07 19:06:34 +0200

    Create app.js
    This file makes the project configurable on Heroku via a single push button. See: https://blog.heroku.com/archives/2014/8/7/heroku-button

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "MPSinatraAppStore",
+  "description": "A Sinatra implementation for a web server that talks with Apple for In-app-purchase-related stuff",
+  "repository": "https://github.com/marzapower/MPSinatraAppStore",
+  "keywords": ["app store", "sinatra", "in-app purchase", "iOS"]
+}
